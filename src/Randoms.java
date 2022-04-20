a3bf38c5db83c50977f782e6d72b24b47b5fa43e
61487 <101150459+61487@users.noreply.github.com>
2022-04-20 16:03:23 +0300

    removed saving all sequence values

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,38 +1,32 @@
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
 
     protected Random random = new Random();
-    private List<Integer> list = new ArrayList<>();
+    private int value;
     private int max;
     private int min;
 
     public Randoms(int min, int max) {
         this.max = max;
         this.min = min;
-        
+
     }
 
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
 
-            int nextIndex = 0;
-
             @Override
             public boolean hasNext() {
-                list.add(random.nextInt((max - min) + 1) + min);
+                value = random.nextInt((max - min) + 1) + min;
                 return true;
             }
 
             @Override
             public Integer next() {
-                int curIndex = nextIndex;
-                nextIndex++;
-                return list.get(curIndex);
+                return value;
             }
         };
     }
